3e9a68b75baf3384e594974f8b0e9800b317e144
Glenn Plas <glenn@byte-consult.be>
2015-04-14 17:05:24 +0200

    small cleanup

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER['APPLICATION_ENV'] == 'development') {
 	error_reporting(E_ALL);
-	ini_set("display_errors", 1);
+	ini_set('display_errors', 1);
 	define('REQUEST_MICROTIME', microtime(true));
 }
 
