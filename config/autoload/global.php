f96b019e0a8f498ee0994fa7894f4b8b27dacf39
Glenn Plas <glenn@byte-consult.be>
2015-04-20 11:31:14 +0200

    phpunit on travis still cant find directors.ini file

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -39,23 +39,28 @@
 $file = "/etc/bareos-webui/directors.ini";
 
 if (!file_exists($file)){
-	echo "No director file found in ".$file.".";
+	echo "No director file found in ".$file.".". PHP_EOL;
 	$file = "./tests/directors.ini";
 }
 
 if (!file_exists($file)){
-	echo "No director file found in ".$file.".";
+	echo "No director file found in ".$file.".". PHP_EOL;
+	$file = "../tests/directors.ini";
+}
+
+if (!file_exists($file)){
+	echo "No director file found in ".$file.".". PHP_EOL;
 	$file = "tests/directors.ini";
 }
 if (!file_exists($file)){
-	echo "No director file found in ".$file.".";
+	echo "No director file found in ".$file.".". PHP_EOL;
 	$file = "directors.ini";
 }
 
 $config = null;
 
 if(!file_exists($file)) {
-	echo "Error: Missing configuration file ".$file.".";
+	echo "Error: Missing configuration file ".$file.".". PHP_EOL;
 	exit();
 } else {
 	$config = parse_ini_file($file, true, INI_SCANNER_NORMAL);
